c0c5a645d4f1d3bfe6a2a3dc56d7f2a793190f53
DuckDood <b.c.youtub@gmail.com>
2026-07-30 17:09:31 +1200

    add rectangles

--- boot.asm
+++ boot.asm
@@ -15,56 +15,100 @@ mov ax,	0x0000
 mov ss,	ax
 mov sp,	0x7c00 ; set up stack pointer and stack offset to be below code (if it overflows it will do bad weird stuff though)
 
-call _clear
-
-mov bx, 0x0
-mov cx, 0x1
-
 main:
-	mov [ds:bx],cx
-	inc bx
-	call _check_screen_end
+	mov al,1
+	call clearscr
 
+	mov ax,320/2 - 100/2
+	mov bx,200/2 - 75/2
+	mov cx,100
+	mov dx,75
+	push 3
+	call rect
+	
+	hlt ; wait for refresh (or something)
+	jmp main
 
-	mov ah, 0x86 ; wait interrupt
+clearscr: ; clear color is in al register
+	push bp
+	mov bp,sp ; create stack frame
+	
+	mov bx,0xA000
+	mov es,bx ; move vga framebuffer offset into extra segment register
+	mov di,0x0 ; clear di (framebuffer starts at 0xA000 + 0 so)
+	mov cx,320*200 ; how many bytes we want to set (the whole buffer)
+	rep stosb ; do it
+
+	mov sp,bp
+	pop bp ; destroy stack frame
+	ret
+
+line: ; ax for x, bx for y, cx for width, stack for color (will stay on stack on return)
+	push bp
+	mov bp,sp ; create stack frame
+
+	push ax
+	push bx
 	push cx
-	push dx ; to get back later after passing params
-	mov cx,0x0
-	;mov dx,16000 ; 16 ms delay (~60fps)
-	mov dx,1
+	push dx
 
-	;int 0x15
+	push ax ; save x coordinate
 
-	times 20 nop
+	mov ax,0xA000
+	mov es,ax ; put framebuffer offset into extra segment
+
+	mov ax,320 ; put screen width in ax
+	mul bx ; multiply bx (row) by ax (screen width) and store it in ax
+	pop bx ; pop x coordinate into bx
+	add ax,bx ; adds bx (x coordinate) to ax (so now ax has the index of the start of the line)
+
+	mov di,ax ; put index into di for rep stosb
+
+	mov al,[bp+4] ; accesses pushed value beforehand for 
+	cld ; make sure this counts in the correct direction
+	rep stosb ; should count down through cx which is already there (we never changed it)
 
 	pop dx
 	pop cx
+	pop bx
+	pop ax
 
-	jmp main
-
-_check_screen_end:
-	cmp bx,320*200
-	ja _reset_screen_fill; jump if above screen bounds
+	mov sp,bp
+	pop bp ; destroy stack frame
 	ret
 
-_reset_screen_fill:
-	xor bx,bx
-	inc cx
+rect: ; ax for x, bx for y, cx for width, dx for height, stack for color
+	push bp
+	mov bp,sp ; create stack frame
+
+	push ax ; keep registers the same after call
+	push bx
+	push cx
+	push dx
+
+	push [bp+4] ; pushes color in stack onto the top of the stack
+
+.rectloop:
+	cmp dx,0
+	je .rectend
+	dec dx
+
+	call line
+	inc bx
+	jmp .rectloop
+
+.rectend:
+
+	pop dx ; just get the top bit off
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+
+	mov sp,bp
+	pop bp ; destroy stack frame
 	ret
 
-_clear:
-	mov bx,320*200
-	jmp _clear_loop
-
-_clear_loop:
-	dec bx
-	mov [bx], 50 ; automatically use ds pointer as offset
-	cmp bx, 0
-	je _clear_end
-	jmp _clear_loop
-
-_clear_end:
-	ret
 
 times 510-($-$$) db 0
-db 0x55, 0xaa
+db 0x55, 0xaa ; in order to make disk bootable
